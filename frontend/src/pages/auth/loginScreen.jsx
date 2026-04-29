c287f48c0917d7f275416425d8a357766c07e044
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2026-04-30 00:13:45 +0545

    Refactor AdminDashboard and LoginScreen components; update images and improve styling

--- frontend/src/pages/auth/loginScreen.jsx
+++ frontend/src/pages/auth/loginScreen.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { useNavigate} from "react-router-dom";
-import coffee from "../../assets/images/coffee.jpg";
+import { useNavigate } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+import logo from "../../assets/images/kitchen_pulse.png";
 
 function LoginScreen() {
   const navigate = useNavigate();
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [msg, setMsg] = useState("");
 
   const handleLogin = () => {
@@ -27,23 +29,26 @@ function LoginScreen() {
   };
 
   return (
-    <div className="bg-[#F5E6D3] min-h-screen flex items-center justify-center">
-      <div className="bg-[#FFF8F1] p-8 rounded-2xl shadow-lg w-100 text-center">
-        <div className="inline-block w-20 h-20 mb-3 rounded-full overflow-hidden">
-          <img
-            src={coffee}
-            alt="logo"
-            className="object-cover w-full h-full"
-          />
+    <div className="bg-white h-screen flex items-center justify-center">
+      <img
+        src="./src/assets/images/piqsels.jpg"
+        alt="image"
+        className="w-full
+        h-full
+        object-cover absolute inset-0 blur-xs"
+      />
+      <div className="backdrop-blur-3xl bg-white/10 border border-white/20 p-8 rounded-2xl shadow-lg w-100 text-center">
+        <div className="inline-block w-20 h-20 mb-6 rounded-full overflow-hidden">
+          <img src={logo} alt="logo" className="object-cover w-full h-full" />
         </div>
 
-        <h1 className="text-xl font-bold mb-2 text-[#4B2E2A]">XYZ Cafe</h1>
+        <p className="text-[16px] font-medium mb-2 text-black">
+          Already have an account? Login
+        </p>
 
-        <h1 className="rounded-lg mb-3 bg-[#F5E6D3] text-[19px] text-[#4B2E2A] font-medium p-2">
-          Login
-        </h1>
-
-        <p className="mb-1 font-medium text-lg text-start text-[#4B2E2A]">Username</p>
+        <p className="mb-1 font-medium text-lg text-start text-[#4B2E2A]">
+          Username
+        </p>
         <input
           type="text"
           placeholder="Username"
@@ -51,30 +56,47 @@ function LoginScreen() {
           className="w-full h-10 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
         />
 
-        <p className="mb-1 font-medium text-lg text-start text-[#4B2E2A]">Password</p>
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full h-10 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
-        />
+        <p className="mb-1 font-medium text-lg text-start text-[#4B2E2A]">
+          Password
+        </p>
+
+        <div className="relative mb-3">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full h-10 border border-[#4B2E2A] bg-white rounded-lg p-2 pr-10"
+          />
+
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600"
+          >
+            {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+          </button>
+        </div>
 
         <p className="mb-5 font-medium text-sm text-red-600">{msg}</p>
 
         <button
           onClick={handleLogin}
           className="text-lg px-4 py-1.5 font-medium
-          bg-[#4B2E2A] text-[#FFF8F1] rounded-lg hover:bg-[#3B221F]">
+          bg-[#4B2E2A] text-[#FFF8F1] rounded-lg hover:bg-[#3B221F]"
+        >
           Login
         </button>
-
+        <hr className="mt-6 " />
         <div className="flex flex-col items-center mt-6 gap-3 mb-1">
-          <p className="text-gray-600 font-medium">Don't have an account? Sign up</p>
-          <button 
-          onClick={()=>navigate("/signup")}
-          className="inline-flex px-4 py-1.5 text-lg font-medium items-center justify-center bg-transparent text-[#4B2E2A] rounded-lg hover:bg-[#F5E6D3] border-2 border-[#4B2E2A]">
-          Sign up
-        </button>
+          <p className="text-black font-medium">
+            Don't have an account? Sign up
+          </p>
+          <button
+            onClick={() => navigate("/signup")}
+            className="inline-flex px-4 py-1.5 text-lg font-medium items-center justify-center bg-transparent text-[#4B2E2A] rounded-lg hover:bg-[#ccc0be] hover:text-[#4B2E2A] border-2 border-[#4B2E2A]"
+          >
+            Sign up
+          </button>
         </div>
       </div>
     </div>
--- frontend/src/pages/auth/signup.jsx
+++ frontend/src/pages/auth/signup.jsx
@@ -1,4 +1,4 @@
-import { UserRoundPlus } from "lucide-react";
+import { UserRoundPlus, Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -10,9 +10,10 @@ function Signup() {
   const [fullname, setFullname] = useState("");
   const [username, setUsername] = useState("");
   const [title, setTitle] = useState("");
-  const [salary, setSalary] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showConfirmPassword, setShowConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [msg, setMsg] = useState("");
 
   const handleSignup = () => {
@@ -25,8 +26,8 @@ function Signup() {
     const usernameRegex = /^[A-Za-z0-9._]+$/;
 
     // Required field check
-    if (!name || !user || !title || !salary || !password || !confirmPassword) {
-      setMsg("All fields are required");
+    if (!name || !user || !title || !password || !confirmPassword) {
+      setMsg("All fields are required*");
     }
 
     // Full name validation (only alphabets + spaces)
@@ -47,45 +48,55 @@ function Signup() {
     // Password match validation
     else if (password !== confirmPassword) {
       setMsg("Passwords do not match");
-    } 
-    
-    else {
+    } else {
       setMsg("");
       navigate("/");
     }
   };
   return (
-    <div className="bg-[#F5E6D3] min-h-screen flex items-center justify-center opacity-[0.9]">
-      <div className="bg-[#FFF8F1] p-8 rounded-2xl shadow-lg w-100 h-170 flex  justify-center items-center">
+    <div className="bg-[#F5E6D3] h-screen flex items-center justify-end">
+      <img
+        src="./src/assets/images/piqsels.jpg"
+        alt="image"
+        className="w-full
+        h-full
+        object-cover absolute inset-0 blur-xs"
+      />
+      <div className="backdrop-blur-3xl bg-white/10 border border-white/20 p-8 rounded-2xl shadow-lg w-100 text-center mr-50">
         <div className=" w-full text-start ">
           <div className="flex items-center justify-start gap-3 mb-5">
-            <UserRoundPlus className="w-5 h-5 text-[#4B2E2A]" />
+            <UserRoundPlus className="w-5 h-5 text-white" />
 
-            <p className="text-[18px] font-bold text-[#4B2E2A]">
+            <p className="text-[18px] font-bold text-white">
               Employee Registration
             </p>
           </div>
 
-          <p className="mb-1 font-medium text-[#4B2E2A]">Full Name*</p>
+          <p className="mb-1 font-medium text-white">Full Name*</p>
           <input
             type="text"
+            placeholder="Eg: Ram Bahadur"
             onChange={(e) => setFullname(e.target.value)}
             className="w-full h-9 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
           />
 
-          <p className="mb-1 font-medium text-[#4B2E2A]">Username*</p>
+          <p className="mb-1 font-medium text-white">Username*</p>
           <input
             type="text"
+            placeholder="Eg: rambahadur"
             onChange={(e) => setUsername(e.target.value)}
             className="w-full h-9 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
           />
 
-          <p className="mb-1 font-medium text-[#4B2E2A]">Job title*</p>
+          <p className="mb-1 font-medium text-white">Job title*</p>
           <select
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className="w-full mb-3 border border-[#4B2E2A] rounded-lg bg-white p-2">
-            <option value="" hidden>Select Job title</option>
+            className="w-full mb-3 border border-[#4B2E2A] rounded-lg bg-white p-2"
+          >
+            <option value="" hidden>
+              Select Job title
+            </option>
             {titles.map((title) => (
               <option key={title} value={title.toLowerCase()}>
                 {title}
@@ -93,38 +104,53 @@ function Signup() {
             ))}
           </select>
 
-          <p className="mb-1 font-medium text-[#4B2E2A]">Salary*</p>
-          <input
-            type="number"
-            onChange={(e) => setSalary(e.target.value)}
-            className="w-full h-9 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
-          />
+          <p className="mb-1 font-medium text-white">Password*</p>
 
-          <p className="mb-1 font-medium text-[#4B2E2A]">Password*</p>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full h-9 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
-          />
+          <div className="mb-3 border border-[#4B2E2A] bg-white rounded-lg flex items-center">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full h-full p-2 rounded-lg"
+            />
 
-          <p className="mb-1 font-medium text-[#4B2E2A]">Confirm Password*</p>
-          <input
-            type="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className="w-full h-9 mb-3 border border-[#4B2E2A] bg-white rounded-lg p-2"
-          />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="text-gray-600 p-2"
+            >
+              {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+            </button>
+          </div>
 
-          <p className="font-medium text-sm mb-3 text-gray-600">
+          <p className="mb-1 font-medium text-white">Confirm Password*</p>
+          <div className="mb-3 border border-[#4B2E2A] bg-white rounded-lg flex items-center ">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="Confirm Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full h-full rounded-lg p-2"
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              className="text-gray-600 p-2"
+            >
+              {showConfirmPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+            </button>
+          </div>
+          <p className="font-medium text-sm mb-3 text-gray-50">
             Note: Every field with * is required
           </p>
 
-          <p className="mb-5 font-medium text-sm text-red-600">{msg}</p>
+          <p className="mb-5 font-medium text-sm text-black">{msg}</p>
 
           <div className="flex justify-between items-center">
             <button
               onClick={handleSignup}
               className="inline-flex w-20 h-9 px-4 py-3 text-[15px] font-medium items-center justify-center 
-            bg-[#4B2E2A] text-white rounded-lg hover:bg-green-400"
+            bg-[#4B2E2A] text-white rounded-lg hover:bg-[#ccc0be] hover:text-[#4B2E2A]"
             >
               Register
             </button>
@@ -132,7 +158,7 @@ function Signup() {
             <button
               onClick={() => navigate("/")}
               className="inline-flex w-20 h-9 px-4 py-3 text-[15px] font-medium items-center justify-center bg-transparent 
-            text-[#4B2E2A] rounded-lg border-2 border-[#4B2E2A] hover:bg-red-400 hover:text-white hover:border-0 "
+            text-white rounded-lg border-2 border-white hover:bg-[#ccc0be] hover:text-[#4B2E2A] hover:border-[#4B2E2A]"
             >
               Cancel
             </button>
@@ -142,4 +168,4 @@ function Signup() {
     </div>
   );
 }
-export default Signup
+export default Signup;
