7f697831d500ccbc92e2ce038771a1adaaba7a3e
saakiyeahwithc <prashantshakya404@gmail.com>
2026-05-09 20:23:49 +0545

    Refactor authentication components and routes; remove unused files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./app/App.jsx";
 import { CafeProvider, UnitProvider } from "./context/cafeContext.jsx";
+import { AuthProvider } from "./auth/authProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-        <CafeProvider>
-          <UnitProvider>
+      <CafeProvider>
+        <UnitProvider>
+          <AuthProvider>
             <App />
-          </UnitProvider>
-        </CafeProvider>
+          </AuthProvider>
+        </UnitProvider>
+      </CafeProvider>
     </BrowserRouter>
   </StrictMode>,
 );
