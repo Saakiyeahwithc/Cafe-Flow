a432a5d5d30ffb04a43b1943e35be5c784e25b19
saakiyeahwithc <prashantshakya404@gmail.com>
2026-05-20 12:57:54 +0545

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { Server } from "lucide-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_URL,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
